850024329db849ce2de89613c3562cafa988ccbd
decard <mail@decard.space>
2025-10-30 14:05:41 +0800

    Update Editor.tsx

--- src/routes/prompts/-promptId/Editor.tsx
+++ src/routes/prompts/-promptId/Editor.tsx
@@ -36,62 +36,69 @@ export const Editor = ({
     ],
     content: prompt.content,
     onUpdate: ({ editor }) => onChange(editor.getHTML()),
+    editable: !prompt.archived,
   });
 
   return (
     <RichTextEditor editor={editor} variant="default">
-      <RichTextEditor.Toolbar sticky stickyOffset="64px">
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.Bold />
-          <RichTextEditor.Italic />
-          <RichTextEditor.Underline />
-          <RichTextEditor.Strikethrough />
-          <RichTextEditor.ClearFormatting />
-          <RichTextEditor.Highlight />
-          <RichTextEditor.Code />
-        </RichTextEditor.ControlsGroup>
+      {prompt.archived ? null : (
+        <RichTextEditor.Toolbar sticky stickyOffset="64px">
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.Bold />
+            <RichTextEditor.Italic />
+            <RichTextEditor.Underline />
+            <RichTextEditor.Strikethrough />
+            <RichTextEditor.ClearFormatting />
+            <RichTextEditor.Highlight />
+            <RichTextEditor.Code />
+          </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.H1 />
-          <RichTextEditor.H2 />
-          <RichTextEditor.H3 />
-          <RichTextEditor.H4 />
-        </RichTextEditor.ControlsGroup>
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.H1 />
+            <RichTextEditor.H2 />
+            <RichTextEditor.H3 />
+            <RichTextEditor.H4 />
+          </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.Blockquote />
-          <RichTextEditor.Hr />
-          <RichTextEditor.BulletList />
-          <RichTextEditor.OrderedList />
-        </RichTextEditor.ControlsGroup>
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.Blockquote />
+            <RichTextEditor.Hr />
+            <RichTextEditor.BulletList />
+            <RichTextEditor.OrderedList />
+          </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.TaskList />
-          <RichTextEditor.TaskListLift />
-          <RichTextEditor.TaskListSink />
-        </RichTextEditor.ControlsGroup>
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.TaskList />
+            <RichTextEditor.TaskListLift />
+            <RichTextEditor.TaskListSink />
+          </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.Link />
-          <RichTextEditor.Unlink />
-        </RichTextEditor.ControlsGroup>
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.Link />
+            <RichTextEditor.Unlink />
+          </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.AlignLeft />
-          <RichTextEditor.AlignCenter />
-          <RichTextEditor.AlignJustify />
-          <RichTextEditor.AlignRight />
-        </RichTextEditor.ControlsGroup>
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.AlignLeft />
+            <RichTextEditor.AlignCenter />
+            <RichTextEditor.AlignJustify />
+            <RichTextEditor.AlignRight />
+          </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.Undo />
-          <RichTextEditor.Redo />
-        </RichTextEditor.ControlsGroup>
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.Undo />
+            <RichTextEditor.Redo />
+          </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.Control ml="auto" style={{ cursor: 'default' }}>
-          {saving ? <Loader size={14} /> : <IconCheck stroke={1.5} size={16} />}
-        </RichTextEditor.Control>
-      </RichTextEditor.Toolbar>
+          <RichTextEditor.Control ml="auto" style={{ cursor: 'default' }}>
+            {saving ? (
+              <Loader size={14} />
+            ) : (
+              <IconCheck stroke={1.5} size={16} />
+            )}
+          </RichTextEditor.Control>
+        </RichTextEditor.Toolbar>
+      )}
 
       <RichTextEditor.Content />
     </RichTextEditor>
