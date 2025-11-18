4f3210bb5f5e1417b8b1f03d62af692c65b407d8
decard <mail@decard.space>
2025-11-19 02:18:56 +0800

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,20 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
-	beforeLoad: async ({ context, location }) => {
-		const role = context.user?.role;
-
-		if (!role) throw redirect({ to: '/login' });
-		else if (location.pathname === '/') {
-			if (role === 'admin')
-				throw redirect({
-					to: '/services',
-					replace: true,
-				});
-			if (role === 'manager')
-				throw redirect({
-					to: '/tasks',
-					replace: true,
-				});
-		}
-	},
+	// beforeLoad: async ({ context, location }) => {
+	// 	const role = context.user?.role;
+	// 	if (!role) throw redirect({ to: '/login' });
+	// 	else if (location.pathname === '/') {
+	// 		if (role === 'admin')
+	// 			throw redirect({
+	// 				to: '/services',
+	// 				replace: true,
+	// 			});
+	// 		if (role === 'manager')
+	// 			throw redirect({
+	// 				to: '/tasks',
+	// 				replace: true,
+	// 			});
+	// 	}
+	// },
 });
