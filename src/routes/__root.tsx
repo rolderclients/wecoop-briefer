5c4b2388d4c99d1a57938a41b854eb7e94f1d702
decard <mail@decard.space>
2025-11-19 17:50:32 +0800

    12

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,18 @@
 import type { QueryClient } from '@tanstack/react-query';
 import { createRootRouteWithContext } from '@tanstack/react-router';
-import { getSessionUser, type SecureUser } from '@/api';
+// import { getSessionUser, type SecureUser } from '@/api';
 import { App } from '@/app';
 import appCss from '../styles.css?url';
 
 export const Route = createRootRouteWithContext<{
 	queryClient: QueryClient;
-	user?: SecureUser;
+	// user?: SecureUser;
 }>()({
-	beforeLoad: async () => {
-		const user = await getSessionUser();
+	// beforeLoad: async () => {
+	// 	const user = await getSessionUser();
 
-		return { user };
-	},
+	// 	return { user };
+	// },
 	head: () => ({
 		meta: [
 			{
