5aab2c1c24e9be180f99bd39399cf8a03c32e337
decard <mail@decard.space>
2025-11-19 15:29:53 +0800

    5

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,40 +1,40 @@
-// import { ColorSchemeScript, MantineProvider } from '@mantine/core';
-// import { Notifications } from '@mantine/notifications';
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 import { HeadContent, Outlet, Scripts } from '@tanstack/react-router';
 import { useEffect } from 'react';
+
 // import { AuthProvider } from './auth';
 // import { Shell } from './Shell';
 
-// const SetTimeZoneCookie = () => {
-// 	useEffect(() => {
-// 		const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-// 		// biome-ignore lint/suspicious/noDocumentCookie: <>
-// 		document.cookie = `tz=${tz}; path=/; max-age=31536000`;
-// 	}, []);
-// 	return null;
-// };
+const SetTimeZoneCookie = () => {
+	useEffect(() => {
+		const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+		// biome-ignore lint/suspicious/noDocumentCookie: <>
+		document.cookie = `tz=${tz}; path=/; max-age=31536000`;
+	}, []);
+	return null;
+};
 
 export const App = () => {
 	return (
 		<html lang="ru" suppressHydrationWarning>
 			<head>
 				<HeadContent />
-				{/*<ColorSchemeScript defaultColorScheme="auto" />*/}
+				<ColorSchemeScript defaultColorScheme="auto" />
 			</head>
 			<body>
-				TEST
-				<Outlet />
-				{/*<MantineProvider defaultColorScheme="auto">
-					<AuthProvider>
-						<Notifications />
-
-						<Shell />
-					</AuthProvider>
+				<MantineProvider defaultColorScheme="auto">
+					{/*<AuthProvider>*/}
+					<Notifications />
+					TEST
+					<Outlet />
+					{/*<Shell />*/}
+					{/*</AuthProvider>*/}
 				</MantineProvider>
 
 				<SetTimeZoneCookie />
 
-				<Scripts />*/}
+				<Scripts />
 			</body>
 		</html>
 	);
