8a8931f488989edf553691cf403c4e2ab076e967
decard <mail@decard.space>
2025-11-20 12:46:31 +0800

    31

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -33,7 +33,7 @@ export const App = () => {
 				{/*</MantineProvider>*/}
 				{/*</AuthProvider>*/}
 				{/*<SetTimeZoneCookie />*/}
-				{/*<Scripts />*/}
+				<Scripts />
 			</body>
 		</html>
 	);
