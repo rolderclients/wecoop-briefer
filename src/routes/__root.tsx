982c3c6f1ac8df22f58c3b2e8b14fdd95a30b61b
decard <mail@decard.space>
2025-11-20 13:02:48 +0800

    34

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,11 +8,12 @@ export const Route = createRootRouteWithContext<{
 	queryClient: QueryClient;
 	user?: SecureUser;
 }>()({
-	// beforeLoad: async () => {
-	// 	const user = await getSessionUser();
+	beforeLoad: async () => {
+		const user = await getSessionUser();
+		console.log('========================', user);
 
-	// 	return { user };
-	// },
+		// return { user };
+	},
 	head: () => ({
 		meta: [
 			{
