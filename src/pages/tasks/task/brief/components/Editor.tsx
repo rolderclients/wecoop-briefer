f2359012bff9ba850b5f640aa66cf07fd93c4fb0
vezdexod <vezdexod@internet.ru>
2025-12-03 16:54:18 +0800

    Пофиксил реакт ошибку с button внутри button, заменив ActionIcon на Icon

--- src/pages/tasks/task/brief/components/Editor.tsx
+++ src/pages/tasks/task/brief/components/Editor.tsx
@@ -3,7 +3,7 @@ import { useChat } from "@/front";
 import { Editor, useEditor } from "~/ui";
 import { useDocument } from "./useDocument";
 import { RichTextEditor } from "@mantine/tiptap";
-import { ActionIcon, Group, Tooltip } from "@mantine/core";
+import { ActionIcon, Group, Loader, Tooltip } from "@mantine/core";
 import { IconFileTypePdf } from "@tabler/icons-react";
 import { useState } from "react";
 import { downloadPDF } from "@/back/functions";
@@ -33,7 +33,19 @@ export const BriefEditor = ({
   return (
     <Editor.Root>
       <Editor.Toolbar saving={saving}>
-        <RichTextEditor.Control style={{ cursor: "pointer" }}>
+        <RichTextEditor.Control
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            setDownloading(true);
+            // console.log("htmlData", editor?.getHTML());
+            downloadPDF(editor?.getHTML() || "", "brief.pdf")
+              .then(() => setDownloading(false))
+              .catch((error) => {
+                console.error("Error downloading PDF:", error);
+                setDownloading(false);
+              });
+          }}
+        >
           <Tooltip
             label="Скачать PDF"
             color="dark"
@@ -42,24 +54,11 @@ export const BriefEditor = ({
             openDelay={100}
             closeDelay={200}
           >
-            <ActionIcon
-              loading={downloading}
-              size={16}
-              variant="transparent"
-              color="green"
-              onClick={() => {
-                setDownloading(true);
-                console.log("htmlData", editor?.getHTML());
-                downloadPDF(editor?.getHTML() || "", "brief.pdf")
-                  .then(() => setDownloading(false))
-                  .catch((error) => {
-                    console.error("Error downloading PDF:", error);
-                    setDownloading(false);
-                  });
-              }}
-            >
-              <IconFileTypePdf stroke={1.5} />
-            </ActionIcon>
+            {downloading ? (
+              <Loader size={14} />
+            ) : (
+              <IconFileTypePdf stroke={1.5} size={16} color="green" />
+            )}
           </Tooltip>
         </RichTextEditor.Control>
       </Editor.Toolbar>
