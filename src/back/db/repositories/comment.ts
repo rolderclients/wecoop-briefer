1a9bec4129f49eb09a9dc8f0692e15979e0cf9ce
vezdexod <vezdexod@internet.ru>
2025-12-11 22:16:23 +0800

    Убрали функцию по удалению комментариев

--- src/back/db/repositories/comment.ts
+++ src/back/db/repositories/comment.ts
@@ -2,7 +2,7 @@ import { createServerFn } from '@tanstack/react-start';
 import { surql } from 'surrealdb';
 import type { CreateComment } from '@/types';
 import { getDB } from '..';
-import { fromDTO, fromDTOs } from '../utils';
+import { fromDTO } from '../utils';
 
 export const createCommentFn = createServerFn({ method: 'POST' })
 	.inputValidator((data: CreateComment) => data)
@@ -11,13 +11,4 @@ export const createCommentFn = createServerFn({ method: 'POST' })
 
 		const content = await fromDTO(data);
 		await db.query(surql`CREATE comment CONTENT ${content};`);
-	});
-
-export const deleteCommentsFn = createServerFn({ method: 'POST' })
-	.inputValidator((data: string[]) => data)
-	.handler(async ({ data }) => {
-		const db = await getDB();
-
-		const ids = await fromDTOs(data);
-		await db.query(surql`FOR $id IN ${ids} { DELETE $id };`);
 	});
