4916885e71c2dfd2e94244f88bedf8f2bf9dc419
vezdexod <vezdexod@internet.ru>
2025-12-11 12:42:33 +0800

    Заменил ScrollArea на mantinовскую

--- src/pages/task/Page.tsx
+++ src/pages/task/Page.tsx
@@ -4,6 +4,7 @@ import {
 	Group,
 	Modal,
 	Paper,
+	ScrollArea,
 	Stack,
 	Text,
 	Title,
@@ -13,14 +14,18 @@ import { IconFileTypePdf } from '@tabler/icons-react';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { useParams } from '@tanstack/react-router';
 import dayjs from 'dayjs';
-import { useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { taskWithBriefAndCommentsQueryOptions } from '@/back';
 import { createCommentFn } from '@/back/db/repositories/comment';
 import { Textarea } from '@/components/ui/textarea';
-import { downloadPDF, SimpleEditor, useMutaitionWithInvalidate } from '@/front';
+import {
+	DefaultNotFoundComponent,
+	downloadPDF,
+	SimpleEditor,
+	useMutaitionWithInvalidate,
+} from '@/front';
 import { Route } from '@/routes/task.$taskId';
 import type { Comment, CreateComment } from '@/types';
-import { ScrollArea } from '~/ui';
 
 export const UnautorizedTaskPage = () => {
 	// Входные данные
@@ -28,8 +33,6 @@ export const UnautorizedTaskPage = () => {
 	const { data: task } = useSuspenseQuery(
 		taskWithBriefAndCommentsQueryOptions(`task:${taskId}`),
 	);
-
-	console.log('taskWithComments', task);
 
 	// Состояния
 	const [newComment, setNewComment] = useState<string>('');
@@ -44,157 +47,182 @@ export const UnautorizedTaskPage = () => {
 		['taskWithBriefAndComments', task.id],
 	);
 
-	return (
-		<Stack pb="xl" pt="sm">
-			<Group justify="flex-end">
-				<Text c="var(--mantine-color-text)" size="xl">
-					Заказчик
-				</Text>
-			</Group>
+	// Скролим вниз, при открытии страницы
+	const viewport = useRef<HTMLDivElement>(null);
 
-			<Grid columns={10} gutter="xl" overflow="hidden">
-				<Grid.Col span={7}>
-					<Stack gap="xs">
-						<Title order={3}>Задача</Title>
+	const scrollToBottom = useCallback(() => {
+		if (viewport.current) {
+			viewport.current.scrollTo({
+				top: viewport.current.scrollHeight,
+				behavior: 'smooth',
+			});
+		}
+	}, []);
 
-						<Paper withBorder radius="md">
-							<Stack px="md" py="sm" h="100%">
-								<Text>{task.title}</Text>
-							</Stack>
-						</Paper>
+	// При добавлении комментариев скролим вниз
+	useEffect(() => {
+		if (task?.comments?.length) scrollToBottom();
+	}, [task.comments, scrollToBottom]);
 
-						<Group justify="space-between" mt="xl">
-							<Title order={3}>Бриф</Title>
+	if (task) {
+		return (
+			<Stack pb="xl" pt="sm">
+				<Group justify="flex-end">
+					<Text c="var(--mantine-color-text)" size="xl">
+						Заказчик
+					</Text>
+				</Group>
 
-							<Group>
-								<Button
-									disabled={true}
-									component="div"
-									size="xs"
-									color="green"
-									variant="light"
-								>
-									Просмотр
-								</Button>
+				<Grid columns={10} gutter="xl" overflow="hidden">
+					<Grid.Col span={7}>
+						<Stack gap="xs">
+							<Title order={3}>Задача</Title>
 
-								<Button
-									loading={downloading}
-									component="div"
-									size="xs"
-									leftSection={<IconFileTypePdf size={16} />}
-									onClick={async () => {
-										setDownloading(true);
-										await downloadPDF(task.brief?.content || '', 'brief.pdf');
-										setDownloading(false);
-									}}
-								>
-									Скачать
-								</Button>
-							</Group>
-						</Group>
+							<Paper withBorder radius="md">
+								<Stack px="md" py="sm" h="100%">
+									<Text>{task.title}</Text>
+								</Stack>
+							</Paper>
 
-						<SimpleEditor
-							height="calc(100vh - 300px)"
-							initialContent={task.brief?.content}
-							initialEditable={false}
-							initialDisabledToolbar
-						/>
-					</Stack>
-				</Grid.Col>
+							<Group justify="space-between" mt="xl">
+								<Title order={3}>Бриф</Title>
 
-				<Grid.Col span={3}>
-					<Stack gap="xs" h="100%">
-						<Title order={3}>Ваши комментарии</Title>
+								<Group>
+									<Button
+										disabled={true}
+										component="div"
+										size="xs"
+										color="green"
+										variant="light"
+									>
+										Просмотр
+									</Button>
 
-						<Paper withBorder radius="md" h="100%">
-							<Stack justify="space-between" h="100%">
-								<ScrollArea
-									autoScroll={true}
-									scrollToBottomOnInit={true}
-									p="xl"
-									h="calc(100vh - 300px)"
-								>
-									<Stack>
-										{task.comments?.map((iComment: Comment) => {
-											return (
-												<Paper
-													key={iComment.id}
-													withBorder
-													radius="md"
-													h="100%"
-												>
-													<Stack gap={0} align="stretch">
-														<Group
-															pt="xs"
-															pr="xs"
-															pl="xs"
-															pb={0}
-															justify="space-between"
-														>
-															<Text c="cyan" size="xs">
-																{task.company.title}
-															</Text>
-															<Text c="cyan" size="xs">
-																{dayjs(iComment.time.created).format(
-																	'DD.MM.YY hh:mm',
-																)}
-															</Text>
-														</Group>
-														<Group p="xs">
-															<Text c="teal" style={{ whiteSpace: 'pre-wrap' }}>
-																{iComment.content}
-															</Text>
-														</Group>
-													</Stack>
-												</Paper>
-											);
-										})}
-									</Stack>
-									<ScrollArea.ScrollButton />
-								</ScrollArea>
-								<Group w="100%" justify="center" mb="50px">
-									<Button radius="xl" bg="green" onClick={open}>
-										Добавить комментарий
+									<Button
+										loading={downloading}
+										component="div"
+										size="xs"
+										leftSection={<IconFileTypePdf size={16} />}
+										onClick={async () => {
+											setDownloading(true);
+											await downloadPDF(task.brief?.content || '', 'brief.pdf');
+											setDownloading(false);
+										}}
+									>
+										Скачать
 									</Button>
 								</Group>
-							</Stack>
-						</Paper>
+							</Group>
+
+							<SimpleEditor
+								height="calc(100vh - 300px)"
+								initialContent={task.brief?.content}
+								initialEditable={false}
+								initialDisabledToolbar
+							/>
+						</Stack>
+					</Grid.Col>
+
+					<Grid.Col span={3}>
+						<Stack gap="xs" h="100%">
+							<Title order={3}>Ваши комментарии</Title>
+
+							<Paper withBorder radius="md" h="100%">
+								<Stack justify="space-between" h="100%">
+									<ScrollArea
+										type="hover"
+										offsetScrollbars={false}
+										p="xl"
+										h="calc(100vh - 300px)"
+										viewportRef={viewport}
+									>
+										<Stack>
+											{task.comments?.map((iComment: Comment) => {
+												return (
+													<Paper
+														key={iComment.id}
+														withBorder
+														radius="md"
+														h="100%"
+													>
+														<Stack gap={0} align="stretch">
+															<Group
+																pt="xs"
+																pr="xs"
+																pl="xs"
+																pb={0}
+																justify="space-between"
+															>
+																<Text c="cyan" size="xs">
+																	{task.company.title}
+																</Text>
+																<Text c="cyan" size="xs">
+																	{dayjs(iComment.time.created).format(
+																		'DD.MM.YY hh:mm',
+																	)}
+																</Text>
+															</Group>
+															<Group p="xs">
+																<Text
+																	c="teal"
+																	style={{ whiteSpace: 'pre-wrap' }}
+																>
+																	{iComment.content}
+																</Text>
+															</Group>
+														</Stack>
+													</Paper>
+												);
+											})}
+										</Stack>
+										{/*<ScrollArea.ScrollButton />*/}
+									</ScrollArea>
+									<Group w="100%" justify="center" mb="50px">
+										<Button radius="xl" bg="green" onClick={open}>
+											Добавить комментарий
+										</Button>
+									</Group>
+								</Stack>
+							</Paper>
+						</Stack>
+					</Grid.Col>
+				</Grid>
+				<Modal
+					size="50%"
+					opened={openedModal}
+					onClose={close}
+					title="Новый комментарий"
+				>
+					<Stack>
+						<Textarea
+							value={newComment}
+							onChange={(event) => setNewComment(event.currentTarget.value)}
+						></Textarea>
+						<Group w="100%" justify="flex-end">
+							<Button
+								loading={createButtonLoading}
+								radius="xl"
+								size="xs"
+								bg="green"
+								onClick={async () => {
+									setCreateButtonLoading(true);
+									await createMutation.mutateAsync({
+										content: newComment,
+										task: task.id,
+									});
+									setNewComment('');
+									setCreateButtonLoading(false);
+									close();
+								}}
+							>
+								Добавить комментарий
+							</Button>
+						</Group>
 					</Stack>
-				</Grid.Col>
-			</Grid>
-			<Modal
-				size="50%"
-				opened={openedModal}
-				onClose={close}
-				title="Новый комментарий"
-			>
-				<Stack>
-					<Textarea
-						value={newComment}
-						onChange={(event) => setNewComment(event.currentTarget.value)}
-					></Textarea>
-					<Group w="100%" justify="flex-end">
-						<Button
-							loading={createButtonLoading}
-							radius="xl"
-							size="xs"
-							bg="green"
-							onClick={async () => {
-								setCreateButtonLoading(true);
-								await createMutation.mutateAsync({
-									content: newComment,
-									task: task.id,
-								});
-								setNewComment('');
-								setCreateButtonLoading(false);
-								close();
-							}}
-						>
-							Добавить комментарий
-						</Button>
-					</Group>
-				</Stack>
-			</Modal>
-		</Stack>
-	);
+				</Modal>
+			</Stack>
+		);
+	} else {
+		return <DefaultNotFoundComponent></DefaultNotFoundComponent>;
+	}
 };
